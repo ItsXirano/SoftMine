43b1c0909c340d45d25f43a148f93263760565b5
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-03-09 18:28:32 -0300

    Update VersionString.php

--- src/softmine/utils/VersionString.php
+++ src/softmine/utils/VersionString.php
@@ -1,25 +1,6 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- * 
- *
-*/
-
-namespace pocketmine\utils;
+namespace softmine\utils;
 
 
 /**
@@ -31,7 +12,7 @@ class VersionString{
 	private $minor;
 	private $development = false;
 
-	public function __construct($version = \pocketmine\VERSION){
+	public function __construct($version = \softmine\VERSION){
 		if(is_int($version)){
 			$this->minor = $version & 0x1F;
 			$this->major = ($version >> 5) & 0x0F;
