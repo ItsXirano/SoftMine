2c8971968b3f92d9b8379388f972f021f82f20db
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-03-10 15:34:51 -0300

    Update TravisTest.php

--- tests/TravisTest.php
+++ tests/TravisTest.php
@@ -1,25 +1,6 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- * 
- *
-*/
-
-$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
+$server = proc_open(PHP_BINARY . " src/softmine/SoftMine.php --no-wizard --disable-readline", [
 	0 => ["pipe", "r"],
 	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
@@ -37,7 +18,7 @@ fclose($pipes[2]);
 
 echo "\n\nReturn value: ". proc_close($server) ."\n";
 
-if(count(glob("plugins/DevTools/PocketMine-MP*.phar")) === 0){
+if(count(glob("plugins/DevTools/SoftMine.phar")) === 0){
 	echo "No server Phar created!\n";
 	exit(1);
 }else{
